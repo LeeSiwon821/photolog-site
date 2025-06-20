9fc3ac422e01674c4deacc6826d260acdf442c49
LeeSiwon821 <your.email@example.com>
2025-06-21 00:21:51 +0900

    처음 업로드

---
+++ script.js
@@ -0,0 +1,26 @@
+var map = L.map('map').setView([35.6, 139.5], 10);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// 오다이바 마커
+var markerOdaiba = L.marker([35.62750390393277, 139.77148816838488]).addTo(map);
+var popupOdaiba = `
+  <div style="width:200px;">
+    <div class="popup-title">시오카제 공원</div>
+    <img src="images/시오카제 공원.jpg" style="width:100%; border-radius:8px;" />
+  </div>
+`;
+markerOdaiba.bindPopup(popupOdaiba);
+
+// 오이시 공원 마커
+var markerOishi = L.marker([35.52290400000001, 138.7457522]).addTo(map);
+var popupOishi = `
+  <div style="width:200px;">
+    <div class="popup-title">오이시 공원</div>
+    <img src="images/오이시공원.jpg" style="width:100%; border-radius:8px;" />
+  </div>
+`;
+markerOishi.bindPopup(popupOishi);
